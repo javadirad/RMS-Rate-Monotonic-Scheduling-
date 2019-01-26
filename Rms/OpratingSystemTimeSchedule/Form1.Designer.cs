848b97e523b3c1277c711a26a694dc3add06a023
mohammad <mohammad.javadirad@gmail.com>
2019-01-26 08:03:24 +0330

    First Version

---
+++ Rms/OpratingSystemTimeSchedule/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace OpratingSystemTimeSchedule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 304);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1159, 257);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(17, 33);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(655, 212);
+            this.listBox4.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(497, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Queue status";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(995, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ongoing work";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(953, 33);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(163, 212);
+            this.listBox2.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1159, 78);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "options";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(183, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Count of Reapeat Process:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(322, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(450, 262);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(250, 47);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(130, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "next Period";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "previus period";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1159, 561);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Scheduling with a RMS algorithm";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label3;
+        private Label label2;
+        private ListBox listBox2;
+        private GroupBox groupBox2;
+        private Button button1;
+        private ListBox listBox4;
+        private GroupBox groupBox3;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private Label label5;
+        private ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Rms/OpratingSystemTimeSchedule/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OpratingSystemTimeSchedule
+{
+
+    public partial class Form1 : Form
+    {
+        private int DelayTime { get; } = 10;
+        public int CurrentTime { get; set; }
+        public int HistoryofCurrentTime { get; set; }
+        private List<Process> _processList = new List<Process>();
+        private readonly Queue _doneJob = new Queue();
+        private readonly List<DoneJobLog> _joblog = new List<DoneJobLog>();
+        public int Curstarttext { get; set; }
+
+        public Form1( )
+        {
+            InitializeComponent();            
+        }
+        private void ReadJobFromFile()
+        {
+            int cnt = 0;
+            Random rnd = new Random();
+            using (var reader = new StreamReader(File.OpenRead("Jobs.csv")))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    
+                    cnt++;
+                    Color randomColor;
+                    switch (cnt)
+                    {
+                        case 1:
+                            randomColor = Color.Blue; break;
+                        case 2:
+                            randomColor = Color.DeepPink; break;
+                        case 3:
+                            randomColor = Color.BlueViolet; break;
+                        case 4:
+                            randomColor = Color.Chartreuse; break;
+                        case 5:
+                            randomColor = Color.DarkGreen; break;
+                        default:
+                            randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                            break;
+                    }
+                    var line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        var values = line.Split(',');
+                        for (int i = 0; i <= Convert.ToInt32(textBox1.Text)-1; i++)
+                        {
+                            Process p1 = new Process(values[0]+(i+1),i* (Convert.ToInt32(values[1]) + Convert.ToInt32(values[2])), 
+                                Convert.ToInt32(values[1]), (i+1) *( Convert.ToInt32(values[1]) + Convert.ToInt32(values[2])),
+                                Convert.ToInt32(values[2]), randomColor);                            
+                            _processList.Add(p1);
+                            listBox4.Items.Add($"processname = {p1.Name} starttime= {p1.Starttime} RemainTime={p1.Remaintime} DeadLineTime={p1.DeadLine}");
+                            listBox4.SelectedIndex = listBox4.Items.Count - 1;
+
+                        } 
+                    }
+                }
+
+            }
+        }
+
+        private void ResetAllObjects()
+        {
+            listBox2.Items.Clear();
+            listBox4.Items.Clear();
+            _processList.Clear();
+            _doneJob.Clear();
+            _joblog.Clear();
+            CurrentTime = 0;
+            HistoryofCurrentTime = 0;
+            groupBox3.Visible = false;
+        }
+        private Process GetProcess()
+        {
+            //_processList = _processList.OrderBy(p => p.Starttime).ThenBy(p=>p.Period).ThenBy(p=>p.Name).ToList();
+            var filteredList = _processList.Where(p => p.Starttime <= CurrentTime).OrderBy(p => p.Period).ThenBy(p => p.Name).ToList();
+            if (filteredList.Count > 0)
+            {
+                Process r = filteredList[0];
+                _processList.Remove(r);
+                return r;
+            }
+
+            return null;
+
+        }
+
+        private void ChangeCurrentTime()
+        {
+            CurrentTime = _processList.OrderBy(p => p.Starttime).First().Starttime;
+        }
+        string CheckProcessField()
+        {
+            if (_processList.Any(p => p.DeadLine < CurrentTime))
+                return $"'{_processList.First(p => p.DeadLine < CurrentTime).Name}'    Field in time {CurrentTime}";
+            return "";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResetAllObjects();
+            ReadJobFromFile();
+            while (true)
+            {
+                System.Threading.Thread.Sleep(DelayTime);
+                if (_processList.Count == 0) break;
+                var p = GetProcess();
+                if (p == null)
+                {
+                    if (_processList.Count == 0) break;
+                    ChangeCurrentTime();
+                    continue;
+                }
+                if (CurrentTime < p.Starttime)
+                    CurrentTime = p.Starttime;
+                DoneJobLog tmp = new DoneJobLog();
+                tmp.Start = CurrentTime;
+                tmp.Processname = p.Name;
+                tmp.PrintColor = p.PrintColor;
+                var currentTime = CurrentTime;
+                p.DoJob(ref currentTime);
+                CurrentTime = currentTime;
+                tmp.End = currentTime;
+                if (CheckProcessField() != "")
+                {
+                    tmp.Hint = CheckProcessField();
+                }
+
+                listBox2.Items.Add(tmp.Processname);
+                listBox2.SelectedIndex = listBox2.Items.Count - 1;
+                _joblog.Add(tmp);
+                Refresh();
+                if (tmp.Hint  !=null)
+                    break;
+            }
+            HistoryofCurrentTime = CurrentTime;
+            groupBox3.Visible = true;
+        }
+
+        private void DrawRotatedTextAt(Graphics gr, float angle,
+            string txt, int x, int y, Font theFont, Brush theBrush)
+        {
+            GraphicsState state = gr.Save();
+            gr.ResetTransform();
+            gr.RotateTransform(angle);
+            gr.TranslateTransform(x, y, MatrixOrder.Append);
+            gr.DrawString(txt, theFont, theBrush, 0, 0);
+            gr.Restore(state);
+        }
+        public void PaintLogs()
+        { 
+        var graphicsObj = CreateGraphics();
+            Pen myPen = new Pen(Color.Black, 3);
+            Pen clearPen = new Pen(BackColor, 3);
+            int startx1 = 100;
+            int starty1 = 200;
+            int startx2 = 1100;
+            int starty2 = 200;
+
+            int startpoint = -15;
+            
+            graphicsObj.DrawLine(myPen, startx1, starty1, startx2, starty2);
+            var myFont = new Font("Tahoma", 8);
+
+            Brush myBrush = new SolidBrush(Color.Red);
+             
+            int starttext =  (CurrentTime / 100) * 100 ;
+            
+            int endtext = ((CurrentTime / 100)+ 1)  * 100;
+
+            if (Curstarttext != starttext)
+            {
+                graphicsObj.DrawLine(clearPen, startx1, starty1+15, startx2, starty2+15);
+            }
+            Curstarttext = starttext;
+            Pen pen1 = new Pen(Color.Blue, 2); 
+
+            DrawRotatedTextAt(graphicsObj, -90, starttext.ToString(), startx1, starty1 + startpoint, myFont, myBrush);
+            DrawRotatedTextAt(graphicsObj, -90, endtext.ToString(), startx2, starty2 + startpoint, myFont, myBrush);
+
+        
+            if (_joblog != null)
+                foreach (var a in _joblog)
+                {
+                    pen1.Color = a.PrintColor; 
+                    if ((a.Start >= starttext && a.Start <= endtext) || (a.End >= starttext && a.End <= endtext))
+                    {
+                        var fStart = a.Start >= starttext ? a.Start : starttext;
+                        var fEnd = a.End <= endtext ? a.End : endtext;
+                     
+                        Brush aBrush = new SolidBrush( pen1.Color );
+                        var x1 = ((startx2 - startx1) / 100) * (fStart % 100) + startx1;
+                        DrawRotatedTextAt(graphicsObj, -90, fStart.ToString(), x1, starty1 + startpoint, myFont,
+                            aBrush);
+                        var x2 = ((startx2 - startx1) / 100) * (fEnd % 100) + startx1;
+                        if (x2 < x1) x2 = startx2;
+                        DrawRotatedTextAt(graphicsObj, -90, fEnd.ToString(), x2, starty1 + startpoint, myFont, aBrush);
+                        graphicsObj.DrawLine( pen1, x1, starty1 + 5, x1, starty1 - 5);
+                        graphicsObj.DrawLine( pen1, x2, starty1 + 5, x2, starty1 - 5);
+
+                        graphicsObj.DrawLine( pen1, x1, starty1 - startpoint, x2,
+                            starty1 - startpoint);
+                        DrawRotatedTextAt(graphicsObj, +90, a.Processname,
+                            x1 + (((startx2 - startx1) / 100) * ((fEnd - fStart) / 2)), starty1 + 20, myFont, aBrush);
+
+                        Brush aHintBrush = new SolidBrush(Color.Red);
+                        if (a.Hint !="")
+                            DrawRotatedTextAt(graphicsObj, +90, a.Hint,
+                               20+ x1 + (((startx2 - startx1) / 100) * ((fEnd - fStart) / 2)), starty1 -80, myFont, aHintBrush);
+                    }
+                }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            PaintLogs();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (CurrentTime < 100) return;
+            CurrentTime -= 100;
+            Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (CurrentTime + 100 > HistoryofCurrentTime) return;
+            CurrentTime += 100;
+            Refresh();
+        }
+    }
+    public class DoneJobLog
+    {
+        public int Start;
+        public int End;
+        public string Processname;
+        public Color PrintColor;
+        public string Hint;
+    }
+    public class Process
+    {
+   
+
+        public Process(string name, int starttime, int runningtime, int deadline, int period,Color printColor)
+        {
+            Runningtime = runningtime;
+            Starttime = starttime;
+            DeadLine = deadline;
+            Remaintime = runningtime;
+            Name = name;
+            Period = period;
+            PrintColor = printColor;
+        }
+        public void DoJob(ref int currentTime)
+        {
+            currentTime = currentTime + Runningtime;
+        }
+        public string Name { get; set; }
+        public int Runningtime { get; set; }
+        public int Remaintime { get; set; }
+        public int Starttime { get; set; }
+        public int DeadLine { get; }
+        public int Period { get; }
+        public Color PrintColor { get; set; }
+    }
+}
